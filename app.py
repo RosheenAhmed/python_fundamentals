091716a05cd4371fce7bf5eb443db73a1f0eb0ee
RosheenAhmed <rosheenahmed2007@gmail.com>
2024-09-01 20:24:09 +0500

    inital commit

---
+++ app.py
@@ -0,0 +1,10 @@
+# write a program that take input from user: name, age, grade, print it on screen
+# name = input(Enter name: ")
+# print("Name is: " + name)
+# print("Age is: "a")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+grade= input ("Enter your grade: ")
+print(f"your Name is: {name}, your Age is: {age}, your Grade is: {grade}")
+
