adf2f599cf31a93ec133346448ea88cbd55df46d
RosheenAhmed <rosheenahmed2007@gmail.com>
2024-09-01 21:38:17 +0500

    logical annd comparision operators

---
+++ comparision_operators.py
@@ -0,0 +1,14 @@
+# > greator than
+# >= grator than equal to
+# < less than
+# <= less than equal to
+# == equal to
+# != not equal to
+
+# note result of these operators are always boolean either True or False
+
+print(10 > 20)
+print(9 < 10)
+print(10 >= 8)
+print(8 == 10)
+print(8 != 8)
---
+++ logical_operators.py
@@ -0,0 +1,10 @@
+# and
+# or
+# not
+
+print(True and True)
+print(False and True)
+print(True or False)
+print(False or False)
+print(not True)
+print(not False)
