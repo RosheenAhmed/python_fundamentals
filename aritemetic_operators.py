beaa650b07cb7f582721c54231445e05bcefcc18
RosheenAhmed <rosheenahmed2007@gmail.com>
2024-09-01 21:24:34 +0500

    arithematic operationrs

---
+++ aritemetic_operators.py
@@ -0,0 +1,28 @@
+# addition +
+# subtraction -
+# multiply *
+# division:
+#     1- / this will give result in floating value
+#     2- // this will give result in integer value
+# modulus %
+# exponential **
+# augumented assingment
+# assignment
+
+num1 = 10
+num2 = 20
+
+sum = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div_float = num1 / num2 # 2.5
+div_int = num1 // num2
+mod = num1 % num2
+expo = num1 ** num2
+
+
+# augumented  assignment example
+x = 10
+y = 1
+x += y # x += y => x = x + y
+print(x)
